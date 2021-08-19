7cdcc67430de3983e9f153e9ce108325b5869b57
sententia7 <winterer02@gmail.com>
2021-08-19 22:53:54 +0900

    블랙잭

--- bruteForce/blackJack.py
+++ bruteForce/blackJack.py
@@ -4,13 +4,26 @@ n, m = map(int, input().split())
 # 카드가 쓰여있는 숫자
 card_number = list(map(int, input().split()))
 
-result = 0
-
+result = []
 for i in range(n):
     for j in range(i+1, n):
         for k in range(j+1, n):
             if card_number[i] + card_number[j] + card_number[k] > m:
                 continue
             else:
-                result = max(result, card_number[i]+card_number[j]+card_number[k])
-print(result)
+                result.append(card_number[i]+card_number[j]+card_number[k])
+print(max(result))
+
+# result = 0
+#
+# for i in range(n):
+#     for j in range(i+1, n):
+#         for k in range(j+1, n):
+#             if card_number[i] + card_number[j] + card_number[k] > m:
+#                 continue
+#             else:
+#                 result = max(result, card_number[i]+card_number[j]+card_number[k])
+# print(result)
+
+
+
